56d88a37c7a7f47474fafc39ae369f9685697857
Antoine <antoine@2ec0b4.fr>
2016-12-04 22:14:57 +0100

    Day 1 solutions

---
+++ Day-01/step1.js
@@ -0,0 +1,25 @@
+var _ = require('lodash'),
+    fs = require('fs');
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    var x = y = 0,
+        directionX = directionY = 1,
+        instructions;
+        
+    if (err) return console.error(err);
+    
+    instructions    = _.split(data, ", ");
+    _.forEach(instructions, function(value, index) {
+        var nb  = ~~_.replace(value, /L|R/, "");
+
+        if (index%2 === 0) {
+            directionX  = ( _.startsWith(value, 'L') ? -1 : 1 ) * directionY;
+            x += directionX * nb;
+        } else {
+            directionY  = ( _.startsWith(value, 'L') ? 1 : -1 ) * directionX;
+            y += directionY * nb;
+        }
+    });
+    
+    console.log("Answer: "+(x+y));
+});
