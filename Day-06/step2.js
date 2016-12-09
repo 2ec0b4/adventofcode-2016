273c07df4566ea4521ffb987c9924d1aa1ce6625
Antoine <antoine@2ec0b4.fr>
2016-12-09 22:47:22 +0100

    Day 6 solutions

---
+++ Day-06/step2.js
@@ -0,0 +1,45 @@
+var _ = require('lodash'),
+    fs = require('fs');
+
+fs.readFile(__dirname+'/input.txt', 'utf8', (err, data) => {
+    var index,
+        nbChar,
+        ranking;
+    
+    if (err) return console.error(err);
+    
+    lines = _.map(_.split(data, "\n"), _.trim);
+    nbChar = lines[0].length;
+    ranking = _.map(new Array(nbChar), function() {
+        return [];
+    });
+    _.forEach(lines, (line) => {
+        index = 0;
+
+        while(index<nbChar) {
+            char = line.charAt(index);
+            if( !ranking[index][char] ) {
+                ranking[index][char] = {
+                    char: char,
+                    count: 0
+                };
+            }
+
+            ranking[index][char].count++;
+            
+            index++;
+        }
+    });
+    
+    ranking = _.map(ranking, function(value) {
+        value   = _.chain(value)
+                    .values()
+                    .orderBy("count", "asc")
+                    .head()
+                    .value();
+            
+        return value.char;
+    });
+    
+    console.log("Answer: "+_.join(ranking, ""));
+});
